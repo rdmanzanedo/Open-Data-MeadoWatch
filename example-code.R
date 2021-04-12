15bd5d0cd5b9501ad7a26ec4357e32e627aeaa37
rdmanzanedo <rdmanzanedo@hotmail.com>
2021-04-12 16:47:18 +0200

    Initial commit for the project

--- example-code.R
+++ example-code.R
@@ -1,16 +1,71 @@
-##new code
+############################DATA PAPER MEADWOWATCH###################
+###DATA COMPILED BY AJI JOHN
+###FIGURES AND THIS ANALYSIS: RUBÉN D. MANZANEDO. APRIL 2021#########
 
-data(iris)
-plot(iris)
-fsdfs
-plot(iris$Sepal.Width)
-mfcolshit shi this
+##WTA data paper potential
+##load the site info for the map
+setwd("C:/Users/rdman/Desktop/Open-Data-MeadoWatch")
+MW_SiteInfo_2013_2020 <- read.csv("MW_SiteInfo_2013_2020.csv")
+library(ggmap)
+library(ggplot2)
 
 
-why on fucking arth
-plot(iris$Species~iris$Petal.Length)
-plot(iris$Petal.Length)
-yes yes no no
-####needs some extra shit here
+#define the edges of the map
+bounds <- c(left = -121.85, 
+            bottom = 46.74,
+            right = -121.5,
+            top = 46.95)
+#download terrain tile
+mtrain <- get_stamenmap(bbox = bounds, maptype = "terrain", zoom = 13)
 
-#end of code
+#display
+map = ggmap(mtrain) +  
+  geom_point(aes(y = Latitude, 
+                 x = Longitude, 
+                 colour = Elevation,
+                 size=2), 
+             data = MW_SiteInfo_2013_2020)
+map+scale_color_gradient(low="blue",high="indianred")
+map+scale_color_gradientn(colours = rainbow(5))
+
+##number of reports per year in both hikes
+#load reportdata
+MW_PhenoDat_2013_2019 <- read.csv("C:/Users/rdman/Desktop/MW_PhenoDat_2013_2019.csv")
+#there multiple reports per year (and potentially per day), subset to observer to make table
+reports.peryear = data.frame('year' = MW_PhenoDat_2013_2019$Year,
+                             'observer' = MW_PhenoDat_2013_2019$Observer,
+                             'transect' = MW_PhenoDat_2013_2019$Transect)
+#remove all other dupolicates
+unique.reports=unique(reports.peryear)
+#number formatting of talbe results
+rep.year = data.frame(table(unique.reports[,-2]))
+rep.year$Freq = as.numeric(rep.year$Freq)
+rep.year$year = 2013:2019
+
+#some graphical parameters
+par(bty="o")
+par(lwd=2)
+par(tcl=-0.2)
+par(las=1)
+par(cex.lab=1.2)
+
+rep.year$Freq[rep.year$Freq==0] = NA
+#plot
+plot(Freq~year, data=subset(rep.year, transect=='Glacier Basin'), 
+     pch=20,
+     type='b', 
+     col='orange',
+     xlim=c(2013,2020),
+     ylim=c(0,100),
+     ylab='Number of reports per year',
+     xlab='Year',
+     lwd=4,
+     cex=3)
+lines(Freq~year, data=subset(rep.year, transect=='Reflection Lakes'), 
+      pch=20,
+      type='b',
+      col= 'dodgerblue4',
+      lwd=4,
+      cex=3)
+text(2019.7,70, 'Glacier Basin', col='orange')
+text(2019.7,85, 'Reflection Lakes', col='dodgerblue4')
