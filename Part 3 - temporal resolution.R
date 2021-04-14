1a2e4bae1f92cd0af48121e2e30d798bbe99e400
rdmanzanedo <rdmanzanedo@hotmail.com>
2021-04-14 16:18:19 +0200

    added figure 3 temporal resolution and script

--- Part 3 - temporal resolution.R
+++ Part 3 - temporal resolution.R
@@ -16,126 +16,69 @@ par(tcl=-0.2)
 par(las=1)
 par(cex.lab=1.2)
 
-###temporal resolution of the database####♥
+###temporal resolution of the database####
 #first turn date to DOY for calculations
 MW_PhenoDat_2013_2019$DOY = strftime(as.Date(MW_PhenoDat_2013_2019$Date, format='%m/%d/%Y'), format = "%j")
 
+##calculate for each hike the number of days in between reports (to get the temporal resolution)
+####first without considering different years#####
+
+#select hikes
 gb.dates = subset(MW_PhenoDat_2013_2019, MW_PhenoDat_2013_2019$Transect=='Glacier Basin')
-rf.dates  = subset(MW_PhenoDat_2013_2019, MW_PhenoDat_2013_2019$Transect=='Reflection Lakes')
-gb.days.with.rep = as.numeric(names(table(gb.dates$DOY)))
-rf.days.with.rep = as.numeric(names(table(rf.dates$DOY)))
-gb.bet.days = diff(gb.days.with.rep, lag=1)
-rf.bet.days = diff(rf.days.with.rep, lag=1)
+rl.dates  = subset(MW_PhenoDat_2013_2019, MW_PhenoDat_2013_2019$Transect=='Reflection Lakes')
 
-##gb
-par(mfcol=c(2,3))
+##GLACIER BASIN#
+
+par(mfrow=c(4,3))
 for (i in 2015:2019){
+  #subset for the each year
   aa = subset(gb.dates, gb.dates$Year==i)
+  #make sure DOY is numeric
   aa.gb.days.with.rep = as.numeric(names(table(aa$DOY)))
+  #calculate difference in dates with previous report
   aa.gb.bet.days = diff(aa.gb.days.with.rep, lag=1)
-  plot(aa.gb.bet.days ~ aa.gb.days.with.rep[-1], 
+  #plot it
+  plot(aa.gb.bet.days~aa.gb.days.with.rep[-1], 
        main=paste(i),
-       pch=21, 
-       col='orange2',
-       bg=rgb(.8,.6,.4,.6),
-       cex=1.5,
-       xlim=c(100,300),
+       pch=16, 
+       col=rgb(.8,.65,.4,.8),
+       cex=2,
+       xlim=c(0,365),
        ylim=c(0,20),
        xlab='Day of the year',
        ylab='Temporal frequency of Meadowatch reports (time since last report)')
-  abline(h=1, lty=2)
-  text(120,2,'Daily')
-  abline(h=7, lty=2)
-  text(120,8,'Weekly')
+  #add reference lines and indications of daily and weekly resolutions
+  abline(h=1, lty=2, lwd=0.5)
+  text(30,2.5,'Daily')
+  abline(h=7, lty=2, lwd=0.5)
+  text(30,9,'Weekly')
   
 }
 
-##reflection
-par(mfcol=c(2,3))
-for (i in 2015:2019){
-  aa = subset(gb.dates, gb.dates$Year==i)
+##REFLECTION LAKE#
+for (i in 2013:2019){
+  #subset for the each year
+  aa = subset(rl.dates, rl.dates$Year==i)
+  #make sure DOY is numeric
   aa.gb.days.with.rep = as.numeric(names(table(aa$DOY)))
+  #calculate difference in dates with previous report
   aa.gb.bet.days = diff(aa.gb.days.with.rep, lag=1)
-  plot(aa.gb.bet.days ~ aa.gb.days.with.rep[-1], 
+  #plot it
+  plot(aa.gb.bet.days ~aa.gb.days.with.rep[-1], 
        main=paste(i),
-       pch=21, 
-       col='orange2',
-       bg=rgb(.8,.6,.4,.6),
-       cex=1.5,
-       xlim=c(100,300),
+       pch=16, 
+       col=rgb(.18,.34,.47,.8),
+       cex=2,
+       xlim=c(0,365),
        ylim=c(0,20),
        xlab='Day of the year',
        ylab='Temporal frequency of Meadowatch reports (time since last report)')
-  abline(h=1, lty=2)
-  text(120,2,'Daily')
-  abline(h=7, lty=2)
-  text(120,8,'Weekly')
+  #add reference lines and indications of daily and weekly resolutions
+  abline(h=1, lty=2, lwd=0.5)
+  text(30,2.5,'Daily')
+  abline(h=7, lty=2, lwd=0.5)
+  text(30,9,'Weekly')
   
 }
 
-
-plot(gb.bet.days ~ gb.days.with.rep[-1], 
-     pch=21, 
-     col='orange2',
-     bg=rgb(.8,.6,.4,.6),
-     cex=1.5,
-     xlim=c(0,366),
-     xlab='Day of the year',
-     ylab='Temporal frequency of Meadowatch reports (time since last report)')
-abline(h=1, lty=2)
-text(20,2,'Daily')
-abline(h=7, lty=2)
-text(20,8,'Weekly')
-abline(h=30, lty=2)
-text(20,31,'Monthly')
-
-
-
-
-
-#change date to DOY 
-
-
-
-#budding graph
-budding.graph = data.frame('year' = MW_PhenoDat_2013_2019$Year,
-                           'transect' = MW_PhenoDat_2013_2019$Transect,
-                           'DOY' = MW_PhenoDat_2013_2019$DOY,
-                           'observer' = MW_PhenoDat_2013_2019$Observer,
-                           'species' = MW_PhenoDat_2013_2019$Species,
-                           'site' = MW_PhenoDat_2013_2019$Site_Code,
-                           'budding' = MW_PhenoDat_2013_2019$Bud,
-                           'flowering' = MW_PhenoDat_2013_2019$Flower,
-                           'fruiting' = MW_PhenoDat_2013_2019$Fruit,
-                           'dispersing' = MW_PhenoDat_2013_2019$Disperse)
-
-head(budding.graph)
-budding2 = unique(budding.graph)
-head(budding2)
-
-
-
-##species - doy - site - n.years.with.budyes
-budding.3.sp1 = data.frame('DOY' = 1:366,
-                           'species' = rep(NA,366),
-                           'site' = rep(NA,366),
-                           'n.years.bud' = rep(NA,366))
-
-
-for
-sitenames=as.character(levels(budding.graph$site))
-
-
-
-species1 = subset(MW_PhenoDat_2013_2019, MW_PhenoDat_2013_2019$Species=='LICA')
-buds.lica = data.frame('DOY' = 1:366,
-                       'n.budding.lica' = rep(NA, 366))
-
-species1.1 = data.frame(species1$)
-?unique
-
-for (i in 1:366){
-  aa = subset(species1, species1$DOY == i)
-  buds.lica$n.budding.lica[i] = dim(aa)[1]
-}
-plot(buds.lica)
+##END OF CODE
