29d7c665c4d56b954aa3feab654e2f31e93f0695
rdmanzanedo <rdmanzanedo@hotmail.com>
2021-04-14 18:24:58 +0200

    added confusion matrix functions and calculation

--- Part 4 - comparison scientists citizens.R
+++ Part 4 - comparison scientists citizens.R
@@ -17,3 +17,84 @@ par(tcl=-0.2)
 par(las=1)
 par(cex.lab=1.2)
 
+#here we have to first pair each scientist obvservation with observations on the very same day (more flexible?)
+
+#split in scientsists and citizens
+scientists = subset(MW_PhenoDat_2013_2019, MW_PhenoDat_2013_2019$QA.QC ==1)
+citizens = subset(MW_PhenoDat_2013_2019, MW_PhenoDat_2013_2019$QA.QC == 0)
+
+#and also in tracks, to pair identical situations
+sci.gb = subset(scientists, scientists$Transect == "Glacier Basin")
+cit.gb = subset(non.scientists, non.scientists$Transect == "Glacier Basin")
+
+sci.rl = subset(scientists, scientists$Transect == "Reflection Lakes")
+cit.rl = subset(citizens, citizens$Transect == "Reflection Lakes")
+
+
+##GLACIER BASIN
+
+#let's keep the variables of importance for each
+sci.assessment = data.frame('Date' = sci.gb$Date,
+                            'Species' = sci.gb$Species,
+                            'Plot' = sci.gb$Site_Code,
+                            'sci.Budding' = sci.gb$Bud,
+                            'sci.Flowering' = sci.gb$Flower,
+                            'sci.Fruiting' = sci.gb$Fruit,
+                            'sci.Seeding' = sci.gb$Disperse)
+non.sci.assessment = data.frame('Date' = non.sci.gb$Date,
+                            'Species' = non.sci.gb$Species,
+                            'Plot' = non.sci.gb$Site_Code,
+                            'cit.Budding' = non.sci.gb$Bud,
+                            'cit.Flowering' = non.sci.gb$Flower,
+                            'cit.Fruiting' = non.sci.gb$Fruit,
+                            'cit.Seeding' = non.sci.gb$Disperse)
+
+#merge removing the incomparables (date, species, and plot need to be identical)
+togetherness = merge(sci.assessment, non.sci.assessment,all = F)
+head(togetherness)
+
+#let's define a function to calculate some main agreement matrices 
+#we can also introduce kappa, if that is more informative
+agreement.sci.cit = function(dataset, phenostate){
+  #define the phenostate to study
+  if(phenostate=='budding') {a=0}
+  if(phenostate=='flowering') {a=1}
+  if(phenostate=='fruiting') {a=2}
+  if(phenostate=='seeding') {a=3}
+  #the a value determines the columns we are checking, as they go in that order
+  #calculate confusion matrix elements and print them
+  SY.CY = dim(subset(dataset, dataset[,4+a] == 1 & dataset[8+a] == 1))[1]
+  SY.CN = dim(subset(dataset, dataset[,4+a] == 1 & dataset[8+a] == 0))[1]
+  SN.CY = dim(subset(dataset, dataset[,4+a] == 0 & dataset[8+a] == 1))[1]
+  SN.CN = dim(subset(dataset, dataset[,4+a] == 0 & dataset[8+a] == 0))[1]
+  print(c('true pos =', SY.CY))
+  print(c('sci.pos.cit.neg = ',SY.CN))
+  print(c('sci.neg.cit.pos = ',SN.CY))
+  print(c('true neg =', SN.CN))
+  
+  #calculate main metrics and print it, here we assume that
+  #scientists define the 'true value'
+  acc = ((SY.CY + SN.CN) / (SY.CY+SY.CN+SN.CY+SN.CN))
+  sen = (SY.CY / (SY.CY+SN.CY))
+  spe = (SN.CN / (SN.CN+SY.CN))
+  print(c('accuracy =', acc))
+  print(c('sensitivity =', sen))
+  print(c('specificity =', spe))
+}
+
+
+agreement.sci.cit(togetherness,'budding')
+agreement.sci.cit(togetherness,'flowering')
+agreement.sci.cit(togetherness,'fruiting')
+agreement.sci.cit(togetherness,'seeding')
+
+
+#we can quickly check the differences between species, for example in flowering id
+
+ls.sp = names(table(togetherness$Species))
+for (i in 1:11){
+  sps = subset(togetherness, togetherness$Species == ls.sp[i])
+  print(ls.sp[i])
+  agreement.sci.cit(sps, 'budding')
+}
+
